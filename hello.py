9d129703e81e400115cb8af0854e53608e0863be
rahul verma <48601181+rahulverma9449@users.noreply.github.com>
2021-12-20 12:43:54 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,3 +3,10 @@ print("python \"future plan\"")
 print("i am working ")
 print("python \"future plan\"")
 print("i am working ")
+
+
+
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
